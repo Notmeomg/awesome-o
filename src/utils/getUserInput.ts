275cc554fd4910ab21f1813d89e5c021811c25fa
Keener <Zachariahkeener@gmail.com>
2023-04-23 22:28:13 -0600

    working on user input

--- src/utils/getUserInput.ts
+++ src/utils/getUserInput.ts
@@ -20,23 +20,67 @@ import chalk from "chalk";
 //   return chalk.gray(d.userInput);
 // };
 
-const getUserInput = (): Promise<string> => {
+// const getUserInput = (): Promise<string> => {
+//   return new Promise((resolve, reject) => {
+//     const rl = readline.createInterface({
+//       input: process.stdin,
+//       output: process.stdout,
+//     });
+
+//     let inputLines: string[] = [];
+
+//     rl.on("line", (line) => {
+//       inputLines.push(line);
+//     });
+
+//     rl.on("close", () => {
+//       resolve(inputLines.join("\n"));
+//     });
+//   });
+// };
+
+function getUserInput(): Promise<string> {
+  // readline.emitKeypressEvents(process.stdin);
+  // if (process.stdin.isTTY) process.stdin.setRawMode(true);
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  process.stdin.on("keypress", (str, key) => {
+    console.log({ str, key });
+    if (key.ctrl && key.name === "c") {
+      // do stuff
+    }
+  });
+
   return new Promise((resolve, reject) => {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-
-    let inputLines: string[] = [];
-
-    rl.on("line", (line) => {
-      inputLines.push(line);
-    });
-
-    rl.on("close", () => {
-      resolve(inputLines.join("\n"));
+    rl.on("line", (input) => {
+      console.log({ input });
+      rl.close();
+      resolve(input);
     });
   });
-};
+}
 
 export default getUserInput;
+
+// let lines: string[] = [];
+
+// for await (const line of rl) {
+//   console.log({ line });
+//   if (line.trim() === "") {
+//     // user pressed Enter
+//     rl.close();
+//     break;
+//   } else if (line.trim() === "\\") {
+//     console.log("shift enter");
+//     // user pressed Shift + Enter
+//     lines.push("");
+//   } else {
+//     lines.push(line);
+//   }
+// }
+
+// return lines.join("\n");
